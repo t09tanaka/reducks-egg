7febb41e57ac06b09deb0b9aa442143f3effb683
Tanaka <t09tanaka@gmail.com>
2020-10-14 19:59:42 +0900

    Remove generated test file

--- test/reducers/forms/account/detail/models.test.ts
+++
@@ -1,9 +0,0 @@
-import AccountDetail from '~/src/reducers/account/detail/models.ts';
-
-test('trim', () => {
-  expect(new AccountDetail().trim({yourValue:''})).toBe({});
-});
-
-test('validate', () => {
-  expect(new AccountDetail().validate({yourValue:''}).noError).toBe(true);
-});
